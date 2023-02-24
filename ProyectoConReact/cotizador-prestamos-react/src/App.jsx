02f7162fcdb80922adea9d46b74b3f5a752945c3
Hyatus <mejia.cristian583@gmail.com>
2023-02-24 00:40:49 -0600

    Cotizador de Pagos con React

--- ProyectoConReact/cotizador-prestamos-react/src/App.jsx
+++ ProyectoConReact/cotizador-prestamos-react/src/App.jsx
@@ -1,13 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header'
 import Button from './components/Button';
-
+import { formatearDinero, calcularTotalPagar } from './helpers';
 
 function App() {
 
   // Aquí deben ir los Hooks
   /*     *State      *Función que lo modifica */
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(()=>{
+      // Cada vez que la cantidad cambie el useEffect se va a ejecutar
+     const resultadoTotalPagar = calcularTotalPagar(cantidad,meses);
+
+     setTotal(resultadoTotalPagar);
+  },[cantidad,meses]);
+
+  useEffect(()=>{
+     // Calcular el pago mensual 
+     setPago(total / meses );
+  },[total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -46,9 +61,11 @@ function App() {
          <div className="flex justify-between my-6">
           <Button 
             operador='-'
+            fn={handleClickDecremento}
           />
           <Button 
             operador='+'
+            fn={handleClickAumento}
           />
          </div>
 
@@ -61,8 +78,33 @@ function App() {
                 value={cantidad}
           />
 
-          <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
+          <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+            {formatearDinero(cantidad)}
+            </p>
 
+          <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+            Elige un <span className="text-indigo-600">plazo</span> a pagar
+          </h2>
+
+          <select className='mt-5 w-full p-2 bg-white border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+          value={meses}
+          onChange={e => setMeses(+e.target.value)}>
+
+            <option value="6">6 meses</option>
+            <option value="12">12 meses</option>
+            <option value="24">24 meses</option>
+          </select>
+
+
+          <div className="my-5 space-y-3 bg-gray-50 p-5">
+            <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+              Resumen <span className="text-indigo-600">de Pagos</span>
+            </h2>
+
+            <p className="text-xl text-gray-500 text-center font-bold">{meses}  Meses</p>
+            <p className="text-xl text-gray-500 text-center font-bold">Total a pagar: {formatearDinero(total)} </p>
+            <p className="text-xl text-gray-500 text-center font-bold">Pagos Mensuales: {formatearDinero(pago)}</p>
+          </div>
       </div>
   )
  
--- ProyectoConReact/cotizador-prestamos-react/src/components/Button.jsx
+++ ProyectoConReact/cotizador-prestamos-react/src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Button({operador}) {
+function Button({operador, fn}) {
 
     return (
         <button className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500"
         type="button"
-        //onClick={funcion}
+        onClick={fn}
         >{operador}</button>
     )
 }
