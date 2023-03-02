1ed9db497b2a73a9cd6f4786f51b361d2cbfaa0c
Hyatus <mejia.cristian583@gmail.com>
2023-03-01 23:50:23 -0600

    Cotizador con Vue

--- ProyectoConVue/cotizador-prestamos-vue/src/components/Button.vue
+++ ProyectoConVue/cotizador-prestamos-vue/src/components/Button.vue
@@ -1,5 +1,8 @@
 <script setup>
     const props = defineProps(['operador','fn']);
+
+
+    const hola = 'Hola';
 </script>
 
 
@@ -7,8 +10,8 @@
     <button
     type="button"
     class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
-    @click="fn"
+    @click="$emit('fn')"
     >
        {{ operador }}
-    </button>
+    </button> 
 </template>
